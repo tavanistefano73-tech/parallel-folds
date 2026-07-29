c3903b8aa8d92a01813e2f089d395fac879671a6
tavanistefano73-tech <tavani.stefano73@gmail.com>
2026-07-29 10:15:16 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 def classFactory(iface):
-    from .parallel_folds_plugin import ParallelFoldsPlugin
+    from .parallel_folds_cross_section import ParallelFoldsPlugin
     return ParallelFoldsPlugin(iface)
